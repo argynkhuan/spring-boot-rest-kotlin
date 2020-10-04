bde6279c17507f7339b5984b3d5d0e04e59c4eba
Argyn Khuan <argynkhuan@yandex.kz>
2020-10-04 20:47:04 +0600

    yaml refactor

--- src/main/kotlin/dev/xuan/springbootkotlin/SpringBootKotlinApplication.kt
+++ src/main/kotlin/dev/xuan/springbootkotlin/SpringBootKotlinApplication.kt
@@ -2,9 +2,18 @@ package dev.xuan.springbootkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class SpringBootKotlinApplication
+@RestController
+class SpringBootKotlinApplication {
+
+	@GetMapping
+	fun index(): String  {
+		return "Hello !"
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringBootKotlinApplication>(*args)
